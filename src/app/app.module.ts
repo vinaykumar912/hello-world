8775e54a8c2507d41719907d6f8239b5a3809d55
vinay <vinay@gmail.com>
2021-03-01 16:50:41 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
